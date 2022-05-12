ade754082ddf78b9e76ca02cda10f8ede57240e2
Kevin Bohorquez <kevintor2246@gmail.com>
2022-05-11 20:18:40 -0500

    Update radiant-dusk.cpp

--- src/c++/temp/radiant-dusk.cpp
+++ src/c++/temp/radiant-dusk.cpp
@@ -3,5 +3,32 @@
     QUE SE USEN PARA COMPILAR LO DE LA RUTA /ADDONS/COMPILE
 
 */
-#include <iostream>  
-int main() { std::cout<<"puedo unirme a tu alianza?"; return 0;}
+
+
+
+/*
+   ¿por que?
+   
+   porque la existencia de esta API REST
+   para compilar c++ de una forma mas sencilla
+   
+   la razon no importa, la cuestion es que esta
+   puede haber sido solo por una simple contribuccion
+   o para darle solucion a cierto problema del pasado
+   
+   es cierto que cosas como la consola hacen parecer
+   que es una deventaja, pero realmente la destinacion
+   de esta API es usar el poder de c++ en toda partes
+   y al alcance de todos y para todos, la documentacion
+   o la estructura puede ser algo confusa pero no es nada del otro mundo,
+   si estas leyendo esto, recuerda que todo esto se 
+   puede utilizar de la manera que quieras esta
+   destinado a libertad total
+
+*/
+
+#include <iostream>
+
+int main(int argc, char *argv[]) { 
+    return 0;
+}
