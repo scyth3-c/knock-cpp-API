91387af5da3c59bfe18b6f65d3b9e27e263d1334
Kevin Bohorquez <kevintor2246@gmail.com>
2022-06-16 13:34:07 -0500

    gen mintilify doc

--- src/controller/crypto.ctrl.ts
+++ src/controller/crypto.ctrl.ts
@@ -3,6 +3,13 @@ import { BinaryToTextEncoding } from "crypto";
 
 const _default = (req:any,res:any):void =>res.json({ruta:"/crypto",info: "knock-api ", });
 
+/**
+ * _sha256 is a function that takes a request, a response, and a next function as arguments and returns
+ * nothing.
+ * @param {any} req - Request object
+ * @param {any} res - The response object.
+ * @param {any} next - The next middleware function in the stack.
+ */
 const _sha256 = (req:any,res:any,next:any):void => {
     const {plain,dg} = req.query
     let sha:SHA256 = new SHA256(plain as string, dg as BinaryToTextEncoding)
