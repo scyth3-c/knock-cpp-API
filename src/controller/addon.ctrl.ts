c32ed74fecb05d205c1ae9af106b6443069b0e6d
Kevin Bohorquez R <kevintor2246@gmail.com>
2022-07-18 18:01:20 -0500

    Update addon.ctrl.ts

--- src/controller/addon.ctrl.ts
+++ src/controller/addon.ctrl.ts
@@ -1,44 +1,44 @@
 import fs from "fs";
 
+
 import * as promise from "./promises/addons";
 
 interface commandeable {
-  body: string;
-  command: string;
-  name: string;
-  useable: boolean;
+  body:string,
+  command: string,
+  name:string
+  useable:boolean,
   addons: {
-    curl: boolean;
-  };
-  bot: boolean;
-  data: DataQuery;
+    curl: boolean
+  }
+  bot: boolean,
+  data: DataQuery,
   optional: {
-    extract: string;
-    data: string;
-  };
-}
+     extract: string,
+     data: string
+  }
+};
 
 interface DataQuery {
-  body: string;
-  name: string;
-}
+  body:string,
+  name:string,
+};
+
 
 /* An interface that is used to create a type for the headers object. */
 interface otherHeaders {
-  title: string;
-  standar: string;
-  O: string;
-  flags: string;
-  bot: boolean;
-}
+    title:string,
+    standar:string,
+    O: string,
+    flags:string,
+    bot: boolean
+};
 
 const _default = (req: any, res: any) => {
-  res.json({
-    ruta: "/addons",
-    endpoints: ["/assembly", "/download", "/compile"],
-    info: "knock-api ",
-  });
+  res.json({ ruta: "/addons", endpoints: ['/assembly', '/download', '/compile'], info: "knock-api " });
 };
+
+
 
 /**
  * It takes the request body, and compiles it into a c++ file, then it runs the file and returns the
@@ -48,121 +48,86 @@ const _default = (req: any, res: any) => {
  * @param {any} next - is a function that is called when the middleware is done.
  */
 const _compile = async (req: any, res: any, next: any) => {
-  let headers: otherHeaders = {
-    title:
-      req.headers["title"] ||
-      (Math.floor(Math.random() * (89894 - 10)) + 10).toString(),
+
+
+  let headers:otherHeaders = {
+    title: req.headers["title"] || (Math.floor(Math.random() * (89894 - 10)) + 10).toString(),
     standar: req.headers["standar"] || "c++17",
     O: req.headers["o"] || "1",
     flags: req.headers["flags"] || "",
-    bot: req.headers["bot"] ? true : false,
+    bot: req.headers["bot"] ? true : false
   };
 
-  let flag_data: string = req.headers["data"];
-  let curl: string = req.headers["curl"];
 
-  let command_data: string = `g++ -std=${headers.standar} ${
-    headers.flags
-  } ${process.cwd()}/src/c++/temp/${headers.title}.cpp -O${
-    headers.O
-  } -o ${process.cwd()}/src/c++/temp/${
-    headers.title
-  }  &&  ${process.cwd()}/src/c++/temp/./${
-    headers.title
-  } < ${process.cwd()}/src/c++/temp/${headers.title}.txt`;
-  let command_raw: string = `g++ -std=${headers.standar} ${
-    headers.flags
-  } ${process.cwd()}/src/c++/temp/${headers.title}.cpp -O${
-    headers.O
-  } -o ${process.cwd()}/src/c++/temp/${
-    headers.title
-  }  &&  ${process.cwd()}/src/c++/temp/./${headers.title}`;
-  let command_make: string = `make title=${headers.title}`;
-  let command_make_data: string = `make title=${headers.title} data=1`;
+    let flag_data:string = req.headers["data"];
+    let curl:string = req.headers["curl"];
 
-  const _getCommand = () => {
-    if (flag_data === undefined && curl != "on") return command_raw;
-    else if (flag_data === undefined && curl == "on") return command_make;
-    else if (flag_data != undefined && curl != "on") return command_data;
-    else if (flag_data != undefined && curl == "on") return command_make_data;
-    else {
-      return command_raw;
-    }
-  };
+    let command_data:string = `g++ -std=${headers.standar} ${headers.flags} ${process.cwd()}/src/c++/temp/${headers.title}.cpp -O${headers.O} -o ${process.cwd()}/src/c++/temp/${headers.title}  &&  ${process.cwd()}/src/c++/temp/./${headers.title} < ${process.cwd()}/src/c++/temp/${headers.title}.txt`;
+    let command_raw:string = `g++ -std=${headers.standar} ${headers.flags} ${process.cwd()}/src/c++/temp/${headers.title}.cpp -O${headers.O} -o ${process.cwd()}/src/c++/temp/${headers.title}  &&  ${process.cwd()}/src/c++/temp/./${headers.title}`;
+    let command_make:string = `make title=${headers.title}  standar=${headers.standar}`;
+    let command_make_data:string = `make title=${headers.title} standar=${headers.standar} data=1`;
 
-  const _getOpt = () => {
-    if (flag_data) {
-      return `${process.cwd()}/src/c++/temp/${headers.title}_bin/./${
-        headers.title
-      } < ${process.cwd()}/src/c++/temp/${headers.title}.txt`;
-    } else {
-      return `${process.cwd()}/src/c++/temp/${headers.title}_bin/./${
-        headers.title
-      }`;
-    }
-  };
+    const _getCommand = () => {
+      if(flag_data === undefined && curl != "on") return command_raw;
+      else if(flag_data === undefined && curl == "on") return command_make;
+      else if(flag_data != undefined && curl != "on") return command_data;
+      else if(flag_data != undefined && curl == "on") return command_make_data; 
+      else {
+        return command_raw;
+      }
+    };  
 
-  let compile: commandeable = {
-    body: req.body,
-    command: _getCommand(),
-    name: `${process.cwd()}/src/c++/temp/${headers.title}.cpp`,
-    useable: flag_data === undefined ? false : true,
-    addons: {
-      curl: curl != "on" ? false : true,
-    },
-    bot: headers.bot,
-    data: {
-      body: flag_data,
-      name: `${process.cwd()}/src/c++/temp/${headers.title}.txt`,
-    },
-    optional: {
-      extract: _getOpt(),
-      data: headers.title,
-    },
-  };
+    const _getOpt = () => {
+      if(flag_data) {
+        return `${process.cwd()}/src/c++/temp/${headers.title}_bin/./${headers.title} < ${process.cwd()}/src/c++/temp/${headers.title}.txt`;
+      } else{
+        return `${process.cwd()}/src/c++/temp/${headers.title}_bin/./${headers.title}`;
+      }
+    };
 
+    let compile:commandeable = {
+        body: req.body,
+        command: _getCommand(),
+        name: `${process.cwd()}/src/c++/temp/${headers.title}.cpp`,
+        useable: flag_data === undefined ? false : true,
+        addons: {
+          curl: curl != "on" ? false : true
+        },
+        bot: headers.bot,
+        data: {
+          body: flag_data,
+          name: `${process.cwd()}/src/c++/temp/${headers.title}.txt`
+        },
+        optional: {
+          extract: _getOpt(),
+          data: headers.title
+        },
+    };
+
+  
   await promise.default
     .compile(compile)
     .then((result) => {
-      if (!compile.addons.curl) {
-        fs.unlink(
-          `${process.cwd()}/src/c++/temp/${headers.title}.cpp`,
-          (err) => {
-            if (err) console.log(err);
-          }
-        );
-        fs.unlink(`${process.cwd()}/src/c++/temp/${headers.title}`, (err) => {
-          if (err) console.log(err);
+
+      if(!compile.addons.curl){
+        fs.unlink(`${process.cwd()}/src/c++/temp/${headers.title}.cpp`, (err)=>{ if(err) console.log(err);
         });
-      } else {
-        fs.unlink(
-          `${process.cwd()}/src/c++/addons/sources/${headers.title}_main/${
-            headers.title
-          }.cpp`,
-          (err) => {
-            if (err) console.log(err);
-          }
-        );
-        fs.rmdir(
-          `${process.cwd()}/src/c++/addons/sources/${headers.title}_main`,
-          (err) => {
-            if (err) console.log(err);
-          }
-        );
+       fs.unlink(`${process.cwd()}/src/c++/temp/${headers.title}`,  (err)=>{ if(err) console.log(err);
+       });
+      } else{
+        fs.unlink(`${process.cwd()}/src/c++/addons/sources/${headers.title}_main/${headers.title}.cpp`,  (err)=>{ if(err) console.log(err);});
+        fs.rmdir(`${process.cwd()}/src/c++/addons/sources/${headers.title}_main`,  (err)=>{ if(err) console.log(err);});
       }
-      if (fs.existsSync(`${process.cwd()}/src/c++/temp/${headers.title}.txt`))
-        fs.unlink(
-          `${process.cwd()}/src/c++/temp/${headers.title}.txt`,
-          (err) => {
-            if (err) console.log(err);
-          }
-        );
+      if(fs.existsSync( `${process.cwd()}/src/c++/temp/${headers.title}.txt`))   fs.unlink(`${process.cwd()}/src/c++/temp/${headers.title}.txt`, (err)=>{ if(err) console.log(err);});
       res.send(result);
     })
     .catch((err) => {
       res.send(err);
     });
 };
+
+
+
 
 /**
  * It downloads a file from the server to the client.
@@ -171,6 +136,7 @@ const _compile = async (req: any, res: any, next: any) => {
  * @param {any} next - any =&gt; next()
  */
 const _download = (req: any, res: any, next: any) => {
+
   let title: string;
   if (req.headers["title"] === undefined) {
     let auc: number = Math.floor(Math.random() * (89894 - 10)) + 10;
@@ -179,19 +145,20 @@ const _download = (req: any, res: any, next: any) => {
     title = req.headers["title"];
   }
   let path = `${process.cwd()}/src/c++/temp/${title}.cpp`;
-  promise.default.download(path, req.body).then((result: any) => {
-    if (result.includes("true")) {
-      res.download(path, () => {
-        if (fs.existsSync(path))
-          fs.unlink(path, (err) => {
-            console.log(err);
-          });
-      });
-    } else {
-      res.send("internal error!");
-    }
-  });
+  promise.default
+    .download(path, req.body)
+    .then((result: any) => {
+      if (result.includes("true")) {
+        res.download(path, ()=>{
+          if(fs.existsSync(path)) fs.unlink(path, (err)=>{console.log(err);})
+        });
+      } else {
+        res.send("internal error!");
+      }
+    });
 };
+
+
 
 /**
  * _asm is a function that takes in a request, response, and next function as parameters. It then
@@ -204,31 +171,27 @@ const _download = (req: any, res: any, next: any) => {
  * @param {any} next - is the next function in the middleware chain
  */
 const _asm = async (req: any, res: any, next: any) => {
-  let headers: otherHeaders = {
-    title:
-      req.headers["title"] ||
-      (Math.floor(Math.random() * (89894 - 10)) + 10).toString(),
+ 
+  let headers:otherHeaders = {
+    title: req.headers["title"] || (Math.floor(Math.random() * (89894 - 10)) + 10).toString(),
     standar: req.headers["standar"] || "c++17",
     O: req.headers["o"] || "1",
     flags: req.headers["flags"] || "",
-    bot: false,
+    bot: false
   };
 
-  let command: string = `g++ -S -std=${
-    headers.standar
-  } ${process.cwd()}/src/c++/temp/${headers.title}_assembly.cpp -O${
-    headers.O
-  } -o ${process.cwd()}/src/c++/temp/${headers.title}_assembly`;
-  let path = `${process.cwd()}/src/c++/temp/${headers.title}_assembly`;
+  let command: string = `g++ -S -std=${headers.standar} ${process.cwd()}/src/c++/temp/${headers.title}_assembly.cpp -O${headers.O} -o ${process.cwd()}/src/c++/temp/${headers.title}_assembly`;
+  let path= `${process.cwd()}/src/c++/temp/${headers.title}_assembly`;
   await promise.default
-    .assembly(command, path + ".cpp", req.body)
+    .assembly(
+      command,
+      path+".cpp",
+      req.body
+    )
     .then((result: any) => {
       if (result.includes("true")) {
-        res.download(path, () => {
-          if (fs.existsSync(path))
-            fs.unlink(path, (err) => {
-              console.log(err);
-            });
+        res.download(path,()=>{
+          if(fs.existsSync(path)) fs.unlink(path, (err)=>{console.log(err);})
         });
       } else {
         res.send(result);
@@ -239,4 +202,6 @@ const _asm = async (req: any, res: any, next: any) => {
     });
 };
 
-export { _compile, _default, _download, _asm };
+
+
+export { _compile, _default, _download, _asm};
